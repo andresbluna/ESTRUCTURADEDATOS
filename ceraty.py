e118c9cf40520b5273114e15b1745a3776dccf5f
andresbluna <andres.benjamin.luna@gmail.com>
2024-03-21 20:28:31 -0300

    first commit

---
+++ ceraty.py
@@ -0,0 +1,7 @@
+x = "La espera me agoto, no se nada de vos, dejaste tanto en mi, En llamas me acosté y en un lento degradé supe que te perdi."
+
+
+def vocal_functions():
+    if x.lower < 0 {
+        
+    }
---
+++ coeficiente_binominal.py
@@ -0,0 +1,8 @@
+def coef_binominal (n,k):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num -1)
+    
+    coef_binominal = factorial(n) // factorial(k)
+
