fb0df10e8095fb2edd9bfa7f3cba71e676bb8e8a
andresbluna <andres.benjamin.luna@gmail.com>
2024-04-04 20:08:22 -0300

    new document C in class

---
+++ vesp01.c
@@ -0,0 +1,16 @@
+/* Hola este es un comentario en c*/
+
+#include<stdio.h>
+
+int main(){
+
+int miNum, miNum2;
+miNum=13;
+miNum2=2;
+
+printf("miNum es igual a: %d\nMinum2 es igual a: %d\n", miNum, miNum2);
+
+return 0;
+
+}
+
