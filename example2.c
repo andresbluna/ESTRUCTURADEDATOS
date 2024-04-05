9c14e531ce4682614db06e6bc1e96d8e24fc4d07
andresbluna <andres.benjamin.luna@gmail.com>
2024-04-05 10:02:55 -0300

    updated progeram file in c

--- example2.c
+++ example2.c
@@ -4,16 +4,23 @@
 int main() {
     int x;
     int y;
+    int modulo = x && y % 2 == 0;
 
     printf("Ingrese un número entero: ");
     scanf("%d", & x);
 
-    if (x && y % 2 == 0) {
+    if(x<1){
+        printf("El numero ingresado no es valido");
+        }
+
+    else if (modulo) {
         printf("El número es par.\n");
     } else {
         printf("El número es impar.\n");
     }
 
     return 0;
+
+    
 }
 
