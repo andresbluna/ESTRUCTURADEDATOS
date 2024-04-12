df73488b42b803e5eab6ad27f4f0d6dcde552b61
andresbluna <andres.benjamin.luna@gmail.com>
2024-04-11 20:05:58 -0400

    Strings on C

--- arreglos.c
+++ arreglos.c
@@ -1,3 +1,19 @@
 /*arreglos unidimensionales*/
+#include<stdio.h>/*standard input output*/
+
+int main() {
+
 int a[10];
+
+a[0] = 2;
+num = a[2];
+
+
+/*Arreglos multidimensionales*/
+
+int matriz[50][50]
+
+
+}
+
 
---
+++ string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    char nombre[20];
+
+    printf("Introduzca su nombre (20 letras maximo)");
+
+
+
+    return 0;
+
+}
